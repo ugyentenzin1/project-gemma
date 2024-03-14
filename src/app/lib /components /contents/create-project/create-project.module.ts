f13ebcdefa26b70103cdf48d2f7bb1c8962eb5e3
Ugyen Tenzin <ugyen.tenzin@selise.ch>
2024-03-14 18:50:12 +0600

    removed unused codes

--- src/app/lib /components /contents/create-project/create-project.module.ts
+++ src/app/lib /components /contents/create-project/create-project.module.ts
@@ -4,7 +4,6 @@ import { CreateProjectComponent } from './create-project.component';
 import { StepsModule } from 'primeng/steps';
 import { ToastModule} from 'primeng/toast';
 import { BasicDetailsComponent } from './basic-details/basic-details.component'
-import { MessageService } from 'primeng/api';
 import { DemographicDetailsComponent } from './demographic-details/demographic-details.component';
 import { ParentsAddressDetailsComponent } from './parents-address-details/parents-address-details.component';
 import { ConfirmationComponent } from './confirmation/confirmation.component';
